c7d781687a18a826a54a0a85c220b7fd74d8a3cf
bendycode <stephen@bendyworks.com>
2014-02-06 18:41:30 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gradioactive::Application.config.secret_key_base = '16338b61ba6b30ae16433198984e99af5fbdf81a4a6b8c2613022bec61a028012bd3e87f84e5c38fe740fb25f2bfb7e67c7cd62c3f72257e23283c8b05c9d28c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gradioactive::Application.config.session_store :cookie_store, key: '_gradioactive_session'
