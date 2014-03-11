d060f8406d4c480bd911e972ddd2d7e930d81d4e
William Tandiawan <william.tandiawan@gmail.com>
2014-03-10 22:08:38 -0500

    Courses model

--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311030457) do
+ActiveRecord::Schema.define(version: 20140311030701) do
 
   create_table "assignments", force: true do |t|
     t.string   "name"
     t.datetime "due"
     t.integer  "points"
     t.text     "instructions"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "courses", force: true do |t|
+    t.string   "name"
+    t.text     "description"
+    t.string   "semester"
+    t.string   "piazza_link"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
